dddfd76103c2d29b79aa20e472cb4f4896cc1b4e
“anar <sehar.uzair23@gmail.com>
2023-04-18 15:20:05 -0300

    tag routes added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,8 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  Product.findAll({include:[Category, Tag]})
-         .then(data =>res.json(data)).catch(err =>res.json(err))
+  Product.findAll({include:[Category, Tag]}).then(productdata =>res.json(productdata)).catch(err =>res.json(err))
 
 });
 
@@ -16,7 +15,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findOne({where:{product_id:req.params.id},include:[Category, Tag]}).then(data =>res.json(data)).catch(err =>res.json(err))
+  Product.findOne({where:{product_id:req.params.id},include:[Category, Tag]}).then(productdata =>res.json(productdata)).catch(err =>res.json(err))
 
 });
 
